56ef5c3713f56a3a9a2f73600d60ca3b829d2714
Hamza EL HARTI (Prest) <hamza.elharti@eurafric-information.com>
2022-05-26 10:52:26 +0100

    handle some exceptions

--- zap_auth_custo.py
+++ zap_auth_custo.py
@@ -196,9 +196,12 @@ class ZapAuthCusto:
         # wait for the page to load
         time.sleep(5)
         logging.info("######### page title: %s",self.driver.title)
-        alert = self.driver.find_element_by_id("reason-text")
-        if proxy_error.find(alert.text) != -1:
-            logging.error("!!!!!!! Cannot access the URL, The proxy blocked the connection !!!!!!")
+        try:
+            alert = self.driver.find_element_by_id("reason-text")
+            if proxy_error.find(alert.text) != -1:
+                logging.error("!!!!!!! Cannot access the URL, The connection was blocked by the proxy !!!!!!")
+        except Exception:
+            logging.error("Login page loaded successfully")
 
         logging.info('automatically finding login elements')
 
